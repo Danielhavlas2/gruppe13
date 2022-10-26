daac602ce13f21a87e45bab5fe7bebcbb9760239
DanielHavlas <daniehav@stud.ntnu.no>
2022-10-26 19:41:28 +0200

    -

--- js/index.js
+++ js/index.js
@@ -1,45 +1,91 @@
 import {ingredienser} from './ingredienser.js'
 
 const startKnapp = document.querySelector('#start-spill')
+const timer = document.querySelector('#timer')
+const poeng = document.querySelector('#poeng')
 const spill = document.querySelector('#spill')
 const kundePizzaE = document.querySelector('#kundePizza')
 const spillRes = document.querySelector('#spillRes')
+const pizza = document.querySelector('#pizza')
 startKnapp.addEventListener('click', startSpill)
+
+ingredienser.forEach(ingrediens => {
+    const ingrKnapp = document.createElement('img')
+    ingrKnapp.setAttribute('class', 'ingrediensKnapp')
+    ingrKnapp.src = `../img/ikoner/${ingrediens}.png`
+    ingrKnapp.addEventListener('click', () => lagPizza(ingrediens))
+    spill.appendChild(ingrKnapp)
+})
 
 let nyPizza = []
 let kundePizza = []
 
+let time = 30;
+timer.innerHTML = time
+let points = 0;
+poeng.innerHTML = points
+
+let pizzaTimeout;
+
 function startSpill() {
+    startKnapp.style.display = 'none'
+    time = 30
+    nyKunde()
+    const spillTimer =  setInterval( () => {
+        time -= 1; 
+        timer.innerHTML = time
+        if (time === 0) {
+            startKnapp.display = 'block'
+            clearInterval(spillTimer)
+            spillRes.innerHTML = 'Tida er ute. Du lagde '  + points + ' pizza. Trykk "Start spill" for å spille igjen.'
+        }  
+    }, 1000);
+
+}
+
+function nyKunde() {
+    nyPizza = []
     kundePizza = nyttPizzaOrdre()
     console.log(kundePizza);
     kundePizzaE.innerHTML = kundePizza
-    setTimeout(() => {
-        kundePizzaE.style.display = 'none'
-        ingredienser.forEach(ingrediens => {
-            const ingrKnapp = document.createElement('button')
-            ingrKnapp.innerHTML = ingrediens
-            ingrKnapp.setAttribute('class', 'ingrediensKnapp')
-            ingrKnapp.addEventListener('click', () => lagPizza(ingrediens))
-            spill.appendChild(ingrKnapp)
-        })
-    },3000)
+    kundePizzaE.style.display = 'block'
+
+    // pizzaTimeout = setTimeout(() => {
+    //     kundePizzaE.style.display = 'none'
+    // },10000)
 }
 
+
 function lagPizza(ingrediens) {
-    nyPizza.push(ingrediens)
-    if(nyPizza.length === kundePizza.length) {
-        const likt = kundePizza.every((v,i) => v === nyPizza[i])
-        console.log(likt);
-        spillRes.innerHTML = likt? 'Du laget rett pizza' : 'Du laget feil pizza' 
+    if(time > 0){
+        nyPizza.push(ingrediens)
+        console.log(ingrediens);
+        const ingr = document.createElement('img')
+        ingr.setAttribute('class', 'ingrediensKnapp')
+        ingr.src = `../img/ikoner/${ingrediens}.png`
+        pizza.appendChild(ingr)
+        if(nyPizza.length === kundePizza.length) {
+            const likt = kundePizza.every((v,i) => v === nyPizza[i])
+            if(likt){
+                points++
+                time += 3
+                poeng.innerHTML = points
+            }else{
+                time -= 2
+            }
+            spillRes.innerHTML = likt? 'Du laget rett pizza' : 'Du laget feil pizza'
+            nyKunde() 
+            pizza.innerHTML = ''
+            // clearTimeout(pizzaTimeout)
+        }
     }
 }
 
 function nyttPizzaOrdre() {
-    let pizzaArray = ['pizzabunn', 'tomatsaus']
+    let pizzaArray = ['bunn', 'saus']
     let tempIngredienser = [...ingredienser]
     tempIngredienser.splice(0,2)
-    console.log(tempIngredienser);
-    const antallIngredienser = Math.ceil(Math.random() * (ingredienser.length-2)+2)
+    const antallIngredienser = Math.ceil(Math.random() * (ingredienser.length-7)) + 1
     for (let i = 0; i < antallIngredienser; i++) {
         const ingrediensIndex = Math.floor(Math.random() * tempIngredienser.length)
         pizzaArray.push(tempIngredienser[ingrediensIndex])
@@ -47,4 +93,3 @@ function nyttPizzaOrdre() {
     }
     return pizzaArray
 }
-
